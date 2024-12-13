3c0a41cc14f6a6374e5789d320636a24c03086e8
ReneeSaulnier <reneesaulnier@outlook.com>
2024-12-12 18:53:17 -0700

    Fix data collection

--- ml/data_collection/main.py
+++ ml/data_collection/main.py
@@ -1,5 +1,7 @@
 import kagglehub
 import yaml
+import os
+import shutil
 
 # Load the config file
 config_path = "config/config.yaml"
@@ -9,5 +11,12 @@ with open(config_path, "r") as file:
 dataset = config["data_collection"]["dataset"]
 download_path = config["data_collection"]["download_path"]
 
-# Download the dataset and save to raw folder
-kagglehub.dataset_download(dataset)
+# Download the dataset (to kagglehub's default cache path)
+cached_path = kagglehub.dataset_download(dataset)
+
+# Move the files from the cache path to the desired local path
+for file_name in os.listdir(cached_path):
+    shutil.move(os.path.join(cached_path, file_name), download_path)
+
+# Print the path to the dataset in the desired folder
+print(f"Dataset files moved to: {download_path}")
