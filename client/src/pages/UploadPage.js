e48ef6195aa2dc305cb806ec8318358319a559a7
ReneeSaulnier <reneesaulnier@outlook.com>
2024-12-20 00:24:24 -0700

    Update upload page

--- client/src/pages/UploadPage.js
+++ client/src/pages/UploadPage.js
@@ -1,31 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { useNavigate } from "react-router-dom";
 import '../styles/UploadPage.css';
 
 function UploadPage() {
 
+  const [fileName, setFileName] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    document.getElementById('file-upload').click();
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setFileName(file.name);
+    }
+  };
+
+  const handleDelete = () => {
+    setFileName('');
+  };
+
+  const handleSubmit = () => {
+    if (fileName) {
+      navigate("/ResultsPage");
+    } else {
+      setError('Please select a file to upload!');
+    }
+  }
+
   return (
     <div className="upload-page">
       <div className="upload-card">
-        <h1 className="upload-title">Upload Photo</h1>
         <div className='upload-dropbox'>
-          <button className="upload-button">
+          <input
+            type="file"
+            id="file-upload"
+            className="input-file"
+            onChange={handleFileChange}
+          />
+          <button className="upload-button" onClick={handleButtonClick}>
             <CloudUploadIcon />
             <span>Upload file</span>
           </button>
-
-          <div className="selected-file">
-            <p>File Name Here</p>
-            <button>
-              <DeleteForeverIcon />
-            </button>
-          </div>
-
+          {fileName && (
+            <div className="selected-file">
+              <p>{fileName}</p>
+              <button onClick={handleDelete}>
+                <DeleteForeverIcon />
+              </button>
+            </div>
+          )}
+          <button className='submit-button' onClick={handleSubmit}>
+            Submit
+          </button>
+          {error && (
+            <p className="error-message">{error}</p>
+          )}
         </div>
       </div>
     </div>
   );
 }
+
 
 export default UploadPage;
