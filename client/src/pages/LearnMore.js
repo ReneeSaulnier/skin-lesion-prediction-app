47633288d779ad4b115ca9b9e5404e99f47619fd
ReneeSaulnier <reneesaulnier@outlook.com>
2024-12-31 23:49:14 -0700

    Add background image

--- client/src/pages/LearnMore.js
+++ client/src/pages/LearnMore.js
@@ -3,12 +3,9 @@ import '../styles/LearnMore.css';
 
 function LearnMore() {
     return (
-        <div>
-            <div className="learn-more">
-                <div className="learn-more-section">
-                <h1 className="title">Learn More.</h1>
-                </div>  
-            </div>
+        <div className="learn-more">
+            <div className="learn-more-section">
+            </div>  
         </div>
     );
     }
